03b7b4998d7d8d8053aee1d5dd420656ad04b533
GirafiStudios <faj10@msn.com>
2023-12-06 03:26:10 +0100

    Make keybinds public

--- common/src/main/java/com/girafi/ping/client/PingKeybinds.java
+++ common/src/main/java/com/girafi/ping/client/PingKeybinds.java
@@ -5,11 +5,11 @@ import org.lwjgl.glfw.GLFW;
 
 public class PingKeybinds {
     private static final String PING_CATEGORY = "ping:key.categories.ping";
-    static final KeyMapping KEY_BINDING = new KeyMapping("key.ping", GLFW.GLFW_KEY_V, PING_CATEGORY);
-    static final KeyMapping PING_ALERT = new KeyMapping("ping.key.alert", GLFW.GLFW_KEY_KP_4, PING_CATEGORY);
-    static final KeyMapping PING_MINE = new KeyMapping("ping.key.mine", GLFW.GLFW_KEY_KP_5, PING_CATEGORY);
-    static final KeyMapping PING_LOOK = new KeyMapping("ping.key.look", GLFW.GLFW_KEY_KP_6, PING_CATEGORY);
-    static final KeyMapping PING_GOTO = new KeyMapping("ping.key.goto", GLFW.GLFW_KEY_KP_8, PING_CATEGORY);
+    public static final KeyMapping KEY_BINDING = new KeyMapping("key.ping", GLFW.GLFW_KEY_V, PING_CATEGORY);
+    public static final KeyMapping PING_ALERT = new KeyMapping("ping.key.alert", GLFW.GLFW_KEY_KP_4, PING_CATEGORY);
+    public static final KeyMapping PING_MINE = new KeyMapping("ping.key.mine", GLFW.GLFW_KEY_KP_5, PING_CATEGORY);
+    public static final KeyMapping PING_LOOK = new KeyMapping("ping.key.look", GLFW.GLFW_KEY_KP_6, PING_CATEGORY);
+    public static final KeyMapping PING_GOTO = new KeyMapping("ping.key.goto", GLFW.GLFW_KEY_KP_8, PING_CATEGORY);
 
     public static class Helper {
         public static boolean lastKeyState = false;
