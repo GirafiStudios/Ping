c2e11c5fc3e4cdd446aba91e459b5062ab1d0e23
GirafiStudios <faj10@msn.com>
2024-08-13 23:55:30 +0200

    Fixed Ping crashing on server startup

--- neoforge/src/main/java/com/girafi/ping/Ping.java
+++ neoforge/src/main/java/com/girafi/ping/Ping.java
@@ -4,12 +4,15 @@ import com.girafi.ping.util.PingConfig;
 import com.girafi.ping.util.PingSounds;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.sounds.SoundEvent;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -19,21 +22,15 @@ public class Ping {
     public static final DeferredRegister<SoundEvent> SOUND_EVENT_DEFERRED = DeferredRegister.create(Registries.SOUND_EVENT, Constants.MOD_ID);
 
     public Ping(ModContainer modContainer, IEventBus eventBus) {
-        eventBus.addListener(this::setupCommon);
-        eventBus.addListener(this::setupClient);
         PingCommon.registerPackets();
-
         modContainer.registerConfig(ModConfig.Type.COMMON, PingConfig.spec);
-        modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+
+        if (FMLEnvironment.dist == Dist.CLIENT) {
+            modContainer.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+        }
 
         registerDeferredRegistries(eventBus);
         SOUND_EVENT_DEFERRED.register("bloop", PingSounds.BLOOP);
-    }
-
-    public void setupCommon(final FMLCommonSetupEvent event) {
-    }
-
-    public void setupClient(final FMLClientSetupEvent event) {
     }
 
     public static void registerDeferredRegistries(IEventBus modBus) {
